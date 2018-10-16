a8c9695f4ca29b62c64a506d46de2147677bb66c
john ornelas <johnnyoh1988@gmail.com>
2018-10-16 14:48:20 -0700

    feat(docker) add options object to database.js

--- database/database.js
+++ database/database.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose');
 const schemas = require('./schemas');
 
-mongoose.connect('mongodb://mongo:27017/test');
+const options = {
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  }
+
+mongoose.connect('mongodb://mongo:27017/test', options);
 const db = mongoose.connection;
+
+// const connectWithRetry = () => {
+//   console.log('MongoDB connection with retry')
+//   mongoose.connect("mongodb://mongo:27017/test", options).then(()=>{
+//     console.log('MongoDB is connected')
+//   }).catch(err=>{
+//     console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+//     setTimeout(connectWithRetry, 5000)
+//   })
+// }
+//
+// connectWithRetry();
+// const db = mongoose.connection;
 
 const init = () => {
   console.log('MONGOOSE IS CONNECTED');
